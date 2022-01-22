c2135a3d2fe687389c1c82e700bee1e899557f18
gulcehocagil <61050489+gulcehocagil@users.noreply.github.com>
2022-01-22 22:24:14 +0300

    Add files via upload

---
+++ yemek/inc/container.php
@@ -0,0 +1,13 @@
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          </ul>
+         
+        </div>
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	
